a2d63a60f224eaeaf1186233b7116773f562743b
Max <maximesin21@gmail.com>
2023-04-05 10:52:15 +0700

    Feature: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <div className="App"></div>;
+  const config = {
+    baseUrl: `https://norma.nomoreparties.space/api/ingredients `,
+  };
+
+  const [state, setState] = useState({
+    data: null,
+    loading: false,
+  });
+
+  useEffect(() => {
+    const getInitialData = () => {
+      setState({ ...state, loading: true });
+      fetch(config.baseUrl)
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Ошибка ${res.status}`);
+        })
+        .then((res) => {
+          setState({ data: res.data, loading: false });
+        });
+    };
+    getInitialData();
+  }, []);
+  console.log(state);
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
